fefe96bcfb3f34f3c5361f78061d8d0480ae1731
k.mesniankin <k.mesniankin@z-it.ru>
2024-10-05 16:48:23 +0300

    feat: refactor environment variables
    BREAKING: Environment variables were changed

--- src/config.ts
+++ src/config.ts
@@ -24,17 +24,17 @@ export const rutrackerUsername = config.get('RUTRACKER_USERNAME', {
 export const rutrackerPassword = config.get('RUTRACKER_PASSWORD', {
   required: true,
 });
-export const qbtWebuiHost = config.get('QBT_WEBUI_HOST', {
+export const qbtWebuiHost = config.get('QBT_WEB_UII_HOST', {
   required: true,
 });
-export const qbtWebuiPort: number = config.get('QBT_WEBUI_PORT', {
+export const qbtWebuiPort: number = config.get('QBT_WEB_UI_PORT', {
   required: true,
   parse: true,
 });
-export const qbtWebuiUsername = config.get('QBT_WEBUI_USERNAME', {
+export const qbtWebuiUsername = config.get('QBT_WEB_UI_USERNAME', {
   required: true,
 });
-export const qbtWebuiPassword = config.get('QBT_WEBUI_PASSWORD', {
+export const qbtWebuiPassword = config.get('QBT_WEB_UI_PASSWORD', {
   required: true,
 });
 export const qbtSavePath = config.get('QBT_SAVE_PATH');
