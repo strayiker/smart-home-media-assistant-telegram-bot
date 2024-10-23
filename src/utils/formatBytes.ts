7cc98f616c004c54c7d1cbeb431bf5ec02603198
k.mesniankin <k.mesniankin@z-it.ru>
2024-10-23 16:24:03 +0300

    fix: lint

--- src/utils/formatBytes.ts
+++ src/utils/formatBytes.ts
@@ -4,7 +4,7 @@ export function formatBytes(bytes: number, decimals = 2) {
   }
 
   const k = 1024;
-  const dm = decimals < 0 ? 0 : decimals;
+  const dm = Math.max(decimals, 0);
   const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
   const size = Number.parseFloat((bytes / Math.pow(k, i)).toFixed(dm));
